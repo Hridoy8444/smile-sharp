ba74a7b8749b1867258ed03d35e4e96de57cc61a
fardous hridoy <mdreday736@gmail.com>
2022-04-18 22:49:20 +0600

    about done

--- src/App.js
+++ src/App.js
@@ -12,6 +12,7 @@ import SuccessFull from './Pages/CheckOut/SuccessFull';
 import Blogs from './Pages/Blogs/Blogs';
 import Footer from './Pages/Shared/Footer/Footer';
 import NotFound from './Pages/NotFound/NotFound';
+import About from './Pages/About/About';
 
 
 
@@ -30,6 +31,7 @@ function App() {
        <Route path='/blogs' element={<Blogs></Blogs>}></Route>
         <Route path='/login' element={<Login></Login>}></Route>
         <Route path='/register' element={<Register></Register>}></Route>
+        <Route path='/about' element={<About></About>}></Route>
         <Route path='*' element={<NotFound></NotFound>}></Route>
       </Routes>
       <Footer></Footer>
