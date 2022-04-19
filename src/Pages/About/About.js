b49c42a7915c00903bb4ed7f9c131c4e0f1d79ce
fardous hridoy <mdreday736@gmail.com>
2022-04-19 21:43:01 +0600

    about updated

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -4,16 +4,18 @@ import './About.css';
 
 const About = () => {
     return (
-        <div style={{minHeight: "70vh"}} className='about-container'>
+        <div style={{ minHeight: "70vh" }} className='about-container'>
             <div >
-                <img height={200} src={about} alt="" />
+                <img height={400} src={about} alt="" />
             </div>
             <div >
-                <h2>Md Fardous Hossain Reday</h2>
-                <h5>Bachelor in Software Engineering, Yangzhou University, China</h5>
+                <div className='text-center' style={{marginTop: "20px"}}>
+                    <h2>Md Fardous Hossain Reday</h2>
+                    <h5>Bachelor in Software Engineering, Yangzhou University, China</h5>
+                </div>
                 <div>
                     <p className='goal-container'>
-                    My goal as a student of software engineering is to be a good wave developer and that is why I chose this course.  Being from an IT background has made things a lot easier for me to learn.  My main goal is to make myself a good developer.  I want to be a full steak developer.  The demand for full steak is current.  It is true that now that many developers are being created, there is a lot of competition.  But there is still a demand for good quality developers.
+                        My goal as a student of software engineering is to be a good wave developer and that is why I chose this course.  Being from an IT background has made things a lot easier for me to learn.  My main goal is to make myself a good developer.  I want to be a full steak developer.  The demand for full steak is current.  It is true that now that many developers are being created, there is a lot of competition.  But there is still a demand for good quality developers.
                     </p>
                 </div>
             </div>
--- src/Pages/Home/Service/Service.js
+++ src/Pages/Home/Service/Service.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import { Button, Card } from 'react-bootstrap';
 import { useNavigate } from 'react-router-dom';
+import './Service.css';
 
 const Service = ({ service }) => {
     const { img, name, price, description } = service;
@@ -11,15 +12,15 @@ const Service = ({ service }) => {
     }
     return (
         <div className='col-12 col-md-6 col-lg-4'>
-            <Card   style={{ width: '18rem', height: "100vh", marginBottom: "20px" }}>
+            <Card   style={{ width: '18rem', height: "95vh", marginBottom: "20px" }}>
                 <Card.Img className='mx-auto w-100 ' style={{width: '260px', height: '230px'}} variant="top" src={img} />
                 <Card.Body>
                     <Card.Title>{name}</Card.Title>
-                    <Card.Text>{price}</Card.Text>
+                    <Card.Text className='text-warning'>Price: {price} $</Card.Text>
                     <Card.Text>
                         {description}
                     </Card.Text>
-                    <Button style={{margin: "0"}} onClick={handleToCheckOut} variant="primary">Go CheckOut</Button>
+                    <Button className='card-btn' onClick={handleToCheckOut} variant="primary">Go CheckOut</Button>
                 </Card.Body>
             </Card>
         </div>
